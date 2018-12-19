a29a29de6575a511d8bd2b01475f1999640d6a6b
Gennadii Donchyts <Gennadii.Donchyts@deltares.nl>
2018-12-19 12:07:21 +0100

    fix build

--- tests/river_test.py
+++ tests/river_test.py
@@ -3,7 +3,7 @@ import json
 import unittest
 import logging
 
-os.environ['key_path'] = '../privatekey.json'
+from . import auth
 
 from hydroengine_service import main
 
@@ -14,14 +14,6 @@ logger = logging.getLogger(__name__)
 
 class TestClient(unittest.TestCase):
     def setUp(self):
-        # if 'privatekey.json' is defined in environmental variable - write it to file
-        if 'key' in os.environ:
-            print('Writing privatekey.json from environmental variable ...')
-            content = base64.b64decode(os.environ['key']).decode('ascii')
-
-            with open('../privatekey.json', 'w') as f:
-                f.write(content)
-
         main.app.testing = True
         self.client = main.app.test_client()
 
