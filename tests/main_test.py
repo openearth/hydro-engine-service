2bfbde620bd226ae77ca225ae419a7492021cf0d
Gennadii Donchyts <Gennadii.Donchyts@deltares.nl>
2018-12-19 11:46:28 +0100

    fix build

--- tests/main_test.py
+++ tests/main_test.py
@@ -14,6 +14,14 @@ logger = logging.getLogger(__name__)
 
 class TestClient(unittest.TestCase):
     def setUp(self):
+        # if 'privatekey.json' is defined in environmental variable - write it to file
+        if 'key' in os.environ:
+            print('Writing privatekey.json from environmental variable ...')
+            content = base64.b64decode(os.environ['key']).decode('ascii')
+
+            with open('../privatekey.json', 'w') as f:
+                f.write(content)
+
         main.app.testing = True
         self.client = main.app.test_client()
 
