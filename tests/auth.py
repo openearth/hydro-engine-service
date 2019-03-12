6dcf63366dd97e6e0f70c0f2fb3167b3bc32ca0d
Fedor Baart <fedor.baart@deltares.nl>
2019-03-12 10:45:05 +0100

    don't overwrite config in test and use paths relative to  file

--- tests/auth.py
+++ tests/auth.py
@@ -1,12 +1,14 @@
 import os
 import base64
 
+import pathlib
+
+
+
 # if 'privatekey.json' is defined in environmental variable - write it to file
 if 'key' in os.environ:
     print('Writing privatekey.json from environmental variable ...')
     content = base64.b64decode(os.environ['key']).decode('ascii')
 
-    with open('../privatekey.json', 'w') as f:
+    with open(pathlib.Path(__file__) / '..' / 'privatekey.json', 'w') as f:
         f.write(content)
-
-os.environ['key_path'] = '../privatekey.json'
