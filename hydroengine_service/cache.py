bc687c94f37848d0d0990c8e00d607f361475667
Fedor Baart <fedor.baart@deltares.nl>
2022-11-03 11:41:34 +0100

    work towards liwo get feature info

---
+++ hydroengine_service/cache.py
@@ -0,0 +1,21 @@
+"""This module provides a global available cache. It can be used for services. It should be initalized with the flask application"""
+import ee
+
+from flask_caching import Cache
+
+cache = Cache()
+
+
+def cache_image(image):
+    """store the json version of the DAG in the cache"""
+    mapid = image.getMapId()["mapid"]
+    serialized = ee.data.serializer.toJSON(image)
+    # store the dag in the cache
+    cache.set(mapid, serialized)
+
+
+def image_from_cache(mapid):
+    """get an image based on the DAG stored in the cache"""
+    serialized = cache.get(mapid)
+    deserialized = ee.deserializer.fromJSON(serialized)
+    return deserialized
